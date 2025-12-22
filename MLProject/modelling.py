6bde86080553282a9ff68779a5587711bccea330
Achmad Firza Fuadi <Achmad_Firza.Fuadi@etu.sorbonne-universite.fr>
2025-12-22 22:18:44 +0100

    error argument

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import importlib.util as importlib_util
 
 if __name__ == "__main__":
-    file_path = sys.argv[3] if len(sys.argv) > 3 else os.path.join(os.path.dirname(os.path.abspath(__file__)), "email_text_preprocessed.csv")
+    file_path = sys.argv[4] if len(sys.argv) > 4 else os.path.join(os.path.dirname(os.path.abspath(__file__)), "email_text_preprocessed.csv")
 
     data = pd.read_csv(file_path)
     data = data.dropna().drop_duplicates()
@@ -35,20 +35,18 @@ if __name__ == "__main__":
     ngram_range = (1, int(sys.argv[2])) if len(sys.argv) > 2 else (1, 1)
     c = float(sys.argv[3]) if len(sys.argv) > 3 else 1.0
 
-    with mlflow.start_run():
+    svc = Pipeline(
+        steps=[
+            ("tfidf", TfidfVectorizer(max_features=max_features, ngram_range=ngram_range)),
+            ("svc", LinearSVC(C=c))
+        ]
+    )
+    svc.fit(X_train, y_train)
 
-        svc = Pipeline(
-            steps=[
-                ("tfidf", TfidfVectorizer(max_features=max_features, ngram_range=ngram_range)),
-                ("svc", LinearSVC(C=c))
-            ]
-        )
-        svc.fit(X_train, y_train)
+    mlflow.sklearn.log_model(sk_model=svc, name="linear_svc", input_example=input_example)
+    y_pred = svc.predict(X_test)
 
-        mlflow.sklearn.log_model(sk_model=svc, name="linear_svc", input_example=input_example)
-        y_pred = svc.predict(X_test)
-
-        mlflow.log_metric("f1_score", f1_score(y_test, y_pred))
-        mlflow.log_metric("accuracy", accuracy_score(y_test, y_pred))
-        mlflow.log_metric("precision", precision_score(y_test, y_pred))
-        mlflow.log_metric("recall", recall_score(y_test, y_pred))
+    mlflow.log_metric("f1_score", f1_score(y_test, y_pred))
+    mlflow.log_metric("accuracy", accuracy_score(y_test, y_pred))
+    mlflow.log_metric("precision", precision_score(y_test, y_pred))
+    mlflow.log_metric("recall", recall_score(y_test, y_pred))
